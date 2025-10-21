c6b75d28a266d28a681270e141dbf5df502ffef9
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2025-10-20 22:58:23 -0500

    created blog form ui

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //prevents default behavior of form submission which would refresh the page
+        const blog = { title, body, author }; //json server autopopulates id property on post request
+
+        console.log(blog);
+    }
+
     return ( 
-        <div className="Create">
+        <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)} //updates title state variable whenever input field changes
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <label>Blog author: </label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add blog</button>
+            </form>
         </div>
      );
 }
