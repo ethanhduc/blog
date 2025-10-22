25befade54401d2423277821352743af4fdf4809
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2025-10-22 17:31:38 -0500

    delete blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams(); //useParams is a hook that returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>.
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/'); //navigate back to home page after deletion
+        })
+    };
 
     return ( 
         <div className="blog-details">
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2> {blog.title}</h2>
                     <p> Written by {blog.author} </p>
                     <div> {blog.body} </div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
